c7f4f35c6b62dd44ffe714573e9a6da2ccd9e26e
santiagoMosquera <zantycb89@gmail.com>
2022-04-05 13:01:26 -0500

    espacio arreglo

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ global.transacciones=[
 	{ monto:50.0, tipo: "R", fecha: "05/04/2022", hora: "12:45", codigo:"VFSSS", nombre: "Ricardo Lopez", id: "174223423" },
 	{ monto:40.0, tipo: "E", fecha: "05/04/2022", hora: "13:45", codigo:"CDASF", nombre: "Rosa Beltrán", id: "0976123123" },
 ]
+
 export default function App() {
   return (
     <View style={styles.container}>
